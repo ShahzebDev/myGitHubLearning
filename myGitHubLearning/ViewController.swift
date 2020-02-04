9d010b59009922f726995dfa3090667142304577
shahzeb yousaf <shahzeby68@gmail.com>
2020-02-04 16:05:43 +0500

    Add Button

--- myGitHubLearning/ViewController.swift
+++ myGitHubLearning/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickedButton(_ sender: Any) {
+        print("This is my Git Learning")
+    }
+    
 }
 
